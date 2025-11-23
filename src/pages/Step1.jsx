7d1d3194702d1d02aaa14720b67b3f1237dd412f
israel_dev <cellbriky12@gmail.com>
2025-11-23 01:28:55 +0100

    update

--- src/pages/Step1.jsx
+++ src/pages/Step1.jsx
@@ -30,8 +30,8 @@ const Step1 = () => {
         <div class='flex  flex-col justify-between h-full bg-[var(--White)] h-full w-full border-0 rounded-xl px-6 py-8' >
             <h1 class='text-2xl font-bold text-[var(--primaryBlue950)] mb-2'>Personal Info</h1>
             <div class='block'>
-              <p class='text-sm font-medium text-[var(--Grey500)]'>Please provide your name, email</p>
-              <p class='text-sm font-medium text-[var(--Grey500)]'>address, and phone number.</p>
+              <p class='text-base font-medium text-[var(--Grey500)]'>Please provide your name, email</p>
+              <p class='text-base font-medium text-[var(--Grey500)]'>address, and phone number.</p>
             </div>
           <form onSubmit={handleSubmit(submit)}  >
 
@@ -40,11 +40,11 @@ const Step1 = () => {
               <input id='name' type='text' placeholder='e.g Stephen King'/>
             </div>
             <div class='mt-2'>
-              <label class='text-xs text-[var(--primaryBlue950)] font-medium' for='email'>Email Address</label>
+              <label class='text-sm text-[var(--primaryBlue950)] font-medium' for='email'>Email Address</label>
               <input id='email' type='text' required placeholder='e.g stephenking@gmail.com'/>
             </div>
             <div class='mt-2'>
-              <label class='text-xs text-[var(--primaryBlue950)] font-medium' for='phone_no'>Phone Number</label>
+              <label class='text-sm text-[var(--primaryBlue950)] font-medium' for='phone_no'>Phone Number</label>
               <input  id='phone_no' type='text' placeholder=' e.g +1 234 567 890'></input>
             </div>
           </form>
